3e8b9b9e4d537993da620f7ce6c53a15c6e53541
shaima-abdelbaset <shaimaaabdelbasset1@gmail.com>
2024-02-16 02:24:54 +0200

    Added the files

---
+++ src/CommandProcessor.java
@@ -0,0 +1,47 @@
+/**
+ * The purpose of this class is to parse a text file into its appropriate, line
+ * by line commands for the format specified in the project spec.
+ * 
+ * @author CS Staff
+ * 
+ * @version 2021-08-23
+ */
+public class CommandProcessor {
+
+    // the database object to manipulate the
+    // commands that the command processor
+    // feeds to it
+    private Database data;
+
+    /**
+     * The constructor for the command processor requires a database instance to
+     * exist, so the only constructor takes a database class object to feed
+     * commands to.
+     * 
+     * @param dataIn
+     *            the database object to manipulate
+     */
+    public CommandProcessor() {
+        data = new Database();
+    }
+
+
+    /**
+     * This method identifies keywords in the line and calls methods in the
+     * database as required. Each line command will be specified by one of the
+     * keywords to perform the actions within the database required. These
+     * actions are performed on specified objects and include insert, remove,
+     * regionsearch, search, intersections, and dump. If the command in the file line is not
+     * one of these, an appropriate message will be written in the console. This
+     * processor method is called for each line in the file. Note that the
+     * methods called will themselves write to the console, this method does
+     * not, only calling methods that do.
+     * 
+     * @param line
+     *            a single line from the text file
+     */
+    public void processor(String line) {
+        
+    }
+
+}
